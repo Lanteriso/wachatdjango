fa425b07b2d10cdda28ca754f0f2bb976ff4386a
janyroo <214778990@qq.com>
2019-03-15 12:29:48 +0800

    heroku提交

--- wxjd/settings.py
+++ wxjd/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
     'wechat',
 ]
 AUTH_USER_MODEL = 'users.User'
